f4b429de55619b122ff1d5181500c3698e19b4c5
JMZAM <juanmazam@gmail.com>
2019-05-15 23:13:09 -0700

    updating confusion matrix implementation
    now calculating sensitivity/specificity

--- vgg.py
+++ vgg.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 import time
 import os
 import copy
+from utils import *
 plt.ion()
 
 def parse_args():
@@ -49,7 +50,7 @@ def transform_data(data_path, b_size):
                                               data_transforms[x])
                       for x in ['train', 'val']}
     dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=b_size,
-                                                  shuffle=True, num_workers=4)
+                                                  shuffle=True, num_workers=8)
                    for x in ['train', 'val']}
     dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
     class_names = image_datasets['train'].classes
@@ -73,7 +74,7 @@ def imshow(dataloaders, class_names, inp):
     plt.pause(0.001)  # pause a bit so that plots are updated
     return
 
-def train_model(model, criterion, optimizer, scheduler, num_epochs, dataloaders, device, dataset_sizes):
+def train_model(model, criterion, optimizer, scheduler, num_epochs, dataloaders, device, dataset_sizes, class_names):
     since = time.time()
 
     best_model_wts = copy.deepcopy(model.state_dict())
@@ -93,7 +94,8 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs, dataloaders,
 
             running_loss = 0.0
             running_corrects = 0
-
+            running_labels = []
+            running_preds = []
             # Iterate over data.
             for inputs, labels in dataloaders[phase]:
                 inputs = inputs.to(device)
@@ -117,17 +119,22 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs, dataloaders,
                 # statistics
                 running_loss += loss.item() * inputs.size(0)
                 running_corrects += torch.sum(preds == labels.data)
-
+                running_labels.extend(list(labels.data.cpu()))
+                running_preds.extend(list(preds.cpu()))
             epoch_loss = running_loss / dataset_sizes[phase]
             epoch_acc = running_corrects.double() / dataset_sizes[phase]
-
+            cm = makeConfusionMatrix(np.array(running_labels, dtype=np.int32), np.array(running_preds, dtype=np.int32))
+            print(cm)
             print('{} Loss: {:.4f} Acc: {:.4f}'.format(
                 phase, epoch_loss, epoch_acc))
-
+            sens, spec = getSensitivityAndSpecificity(cm)
+            print('Sensitivity: {:.4f} Specificity {:.4f}'.format(sens, spec))
             # deep copy the model
             if phase == 'val' and epoch_acc > best_acc:
                 best_acc = epoch_acc
                 best_model_wts = copy.deepcopy(model.state_dict())
+                best_labels = running_labels.copy()
+                best_preds = running_preds.copy()
 
         print()
 
@@ -135,7 +142,8 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs, dataloaders,
     print('Training complete in {:.0f}m {:.0f}s'.format(
         time_elapsed // 60, time_elapsed % 60))
     print('Best val Acc: {:4f}'.format(best_acc))
-
+    print('Confusion matrix')
+    plot_confusion_matrix(makeConfusionMatrix(np.array(best_labels, dtype=np.int32),np.array(best_preds, dtype=np.int32)), class_names)
     # load best model weights
     model.load_state_dict(best_model_wts)
     return model
@@ -160,6 +168,6 @@ def main():
     model_ft, criterion, optimizer_ft, exp_lr_scheduler = makeModel(device, learn_rate,
                                                                     True, out_classes, freeze_weights)
     model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
-                           num_epochs, dataloaders, device, dataset_sizes)
+                           num_epochs, dataloaders, device, dataset_sizes, class_names)
 if __name__ == "__main__":
     main()
